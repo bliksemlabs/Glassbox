4d0d1eae9d0710c16cc32af65206bdda8b466bbb
Thomas Koch <thakoch@gmail.com>
2014-07-30 19:31:00 +0200

    Check in working single-faresection price calucation

--- farecalculator.py
+++ farecalculator.py
@@ -30,20 +30,41 @@ def sections_to_faresections(journey):
 
 def calculate_transferless_fare(fare_sections):
     faresection = fare_sections[0]
-    if faresection['operator'] == 'NS':
-        c = db.cursor()
-        res = c.execute("""
-SELECT distance,price_2ndfull,price_2nd20,price_2nd40,price_1stfull,price_1st20,price_1st40
-FROM distance JOIN fareunit_price USING (distance)
-WHERE from_station = ? AND to_station = ? AND operator = 'NS'""",(faresection['fromStation'],faresection['toStation'])).fetchall()
-        if len(res) == 0:
-            return None #No fare found
-        if len(res) > 1:
-            raise Exception('NS Multiple fares found')
-        c.close()
-        return res
+    c = db.cursor()
+    c.execute("""
+SELECT d.distance,fareunits,price_2ndfull,price_1stfull,
+       (( max(d.distance,d.min_distance) * c.price_first)+entrance_fee)  as totalkmprice_1,
+       (( max(d.distance,d.min_distance) * c.price_second)+entrance_fee) as totalkmprice_2,
+       c.price_first,
+       c.price_second,
+       min_fare
+FROM distance d JOIN fareunit_price fp ON (fp.distance = d.distance OR (fp.distance = 250 AND d.distance > 250))
+                JOIN concession c USING (concession)
+WHERE from_station = ? AND to_station = ? AND operator = ?; """,(faresection['fromStation'],
+                                                                 faresection['toStation'],
+                                                                 faresection['operator'])   )
+    res = c.fetchall()
+    if len(res) == 0:
+        return None #No fare found
+    if len(res) > 1:
+        raise Exception('NS Multiple fares found')
+    distance,fareunits,price_2ndfull,price_1stfull,totalkmprice_1,totalkmprice_2,price_first,price_second,min_fare = res[0]
+    c.close()
+    print res[0]
+    if fareunits:
+        return (distance,price_1stfull,price_2ndfull)
     else:
-        raise Exception('Only NS supported')
+        if min_fare != 'NULL' and min_fare > totalkmprice_1:
+            totalkmprice_1 = min_fare
+        if min_fare != 'NULL' and min_fare > totalkmprice_2:
+            totalkmprice_2 = min_fare
+        if price_first == 'NULL':
+            totalkmprice_1 = None
+        else:
+            totalkmprice_1 = int(round(totalkmprice_1,0))
+        totalkmprice_2 = int(round(totalkmprice_2,0))
+        return (distance,totalkmprice_1,totalkmprice_2)
+
 
 """
 Input a journey, a list of sections. A section is a dict, containing fromStation, toStation, operator.
@@ -68,12 +89,14 @@ def calculate_journey(from_station,to_station):
            reisstops = reisdeel.findall(".//ReisStop")
            section = {'fromStation' : reisstops[0].find('Code').text.lower(),
                       'toStation'   : reisstops[-1].find('Code').text.lower(),
-                      'operator'    : reisdeel.find('Vervoerder').text.replace('Arriva','ARR').replace('Syntus','SYNTUS').replace('Valleilijn','CXX').replace('NS International','NS').replace('GVB','NS').replace('R-net','NS')}
+                      'operator'    : reisdeel.find('Vervoerder').text.replace('Arriva','ARR').replace('Syntus','SYNTUS').replace('Valleilijn','CXX').replace('NS International','NS').replace('GVB','NS').replace('R-net','NS').replace('Veolia','VTN')}
            journey.append(section)
         fare = calculate_fare(journey)
         print '------------------'
         print fare
         for prijs in reismogelijkheid.findall(".//Prijs"):
+            if prijs.get('korting') != '0':
+                continue
             print 'Klasse '+ prijs.get('klasse') + ' Korting '+prijs.get('korting') + ' prijs '+prijs.text
         print '------------------'
             
---
+++ load_data.py
@@ -0,0 +1,113 @@
+import sqlite3
+import os
+
+try:
+    os.remove('data.db')
+except:
+    pass
+
+db = sqlite3.connect('data.db')
+
+c = db.cursor()
+
+c.execute("CREATE TABLE distance ( from_station TEXT, to_station TEXT, distance integer, operator TEXT, min_distance integer);")
+c.execute("CREATE index distance_idx ON DISTANCE (from_station,to_station);")
+for line in open('distance.csv'):
+    values = line[:-1].split(',')
+    if values[4] == '' : values[4] = None
+    c.execute("INSERT INTO distance VALUES (?,?,?,?,?)",values)
+
+c.execute(
+"""
+CREATE TABLE fareunit_price (
+distance integer,
+price_2ndfull integer,
+price_2nd20 integer,
+price_2nd40 integer,
+price_1stfull integer,
+price_1st20 integer,
+price_1st40 integer
+);""")
+c.execute("CREATE INDEX fareunit_price_idx on fareunit_price(distance);")
+for line in open('unitprices.csv'):
+    values = line[:-1].split(',')
+    c.execute("INSERT INTO fareunit_price VALUES (?,?,?,?,?,?,?)",values)
+
+c.execute(
+"""
+CREATE TABLE concession (
+concession TEXT,
+fareunits boolean,
+price_second float,
+price_first float,
+entrance_fee float,
+min_fare integer
+);""")
+c.execute("CREATE INDEX concession_idx ON concession(concession);")
+for line in open('prices.csv'):
+    values = line[:-1].split(',')
+    c.execute("INSERT INTO concession VALUES (?,?,?,?,?,?)",values)
+
+#SET CONCESSIONS
+c.execute("ALTER TABLE distance ADD COLUMN concession TEXT")
+#HRN (NS)
+c.execute("UPDATE distance SET concession = 'HRN' WHERE operator = 'NS'")
+
+#Noordelijke nevenlijnen
+c.execute("""
+UPDATE distance set concession  = 'NOORD' WHERE operator = 'ARR' and (from_station = 'lw' OR to_station = 'lw');""")
+c.execute("""UPDATE distance set concession  = 'NOORD' WHERE operator = 'ARR' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'ARR' and from_station = 'lw');""")
+
+#Zwolle - Emmen Arriva
+c.execute("""
+UPDATE distance set concession  = 'ARR_ZL_EMN' WHERE operator = 'ARR' and (from_station = 'emn' or to_station = 'emn');""")
+c.execute("""UPDATE distance set concession  = 'ARR_ZL_EMN' WHERE operator = 'ARR' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'ARR' and from_station = 'emn');""")
+
+#Zutphen - Apeldoorn Arriva
+c.execute("""
+UPDATE distance set concession  = 'ARR_ZP_APD' WHERE operator = 'ARR' and (from_station = 'apd' or to_station = 'apd');""")
+c.execute("""UPDATE distance set concession  = 'ARR_ZP_APD' WHERE operator = 'ARR' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'ARR' and from_station = 'apd');""")
+
+#MLL Arriva
+c.execute("""
+UPDATE distance set concession  = 'ARR_MLL' WHERE operator = 'ARR' and (from_station = 'ddr' or to_station = 'ddr');""")
+c.execute("""UPDATE distance set concession  = 'ARR_MLL' WHERE operator = 'ARR' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'ARR' and from_station = 'ddr');""")
+
+#Breng
+c.execute("""UPDATE distance set concession  = 'BRENG' WHERE operator = 'BRENG';""")
+
+#Limburg Zuid (Maastricht - Kerkrade)
+c.execute("""
+UPDATE distance set concession  = 'LIM_ZUID' WHERE operator = 'VTN' and (from_station = 'mt' or to_station = 'mt');""")
+c.execute("""UPDATE distance set concession  = 'LIM_ZUID' WHERE operator = 'VTN' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'VTN' and from_station = 'mt');""")
+
+#Limburg Noord (Roermond - Nijmegen)
+c.execute("""
+UPDATE distance set concession  = 'LIM_NOORD' WHERE operator = 'VTN' and (from_station = 'rm' or to_station = 'rm');""")
+c.execute("""UPDATE distance set concession  = 'LIM_NOORD' WHERE operator = 'VTN' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'VTN' and from_station = 'rm');""")
+
+#Valleilijn
+c.execute("""
+UPDATE distance set concession  = 'VALLEI' WHERE operator = 'CXX' and (from_station = 'bnc' or to_station = 'bnc');""")
+c.execute("""UPDATE distance set concession  = 'VALLEI' WHERE operator = 'CXX' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'CXX' and from_station = 'bnc');""")
+
+#Syntus ZOH
+c.execute("""
+UPDATE distance set concession  = 'SYNTUS_ZOH' WHERE operator = 'SYNTUS' AND (to_station = 'hgl' OR from_station = 'hgl');""")
+c.execute("""UPDATE distance set concession  = 'SYNTUS_ZOH' WHERE operator = 'SYNTUS' and to_station in 
+(SELECT DISTINCT to_station  FROM distance WHERE operator = 'SYNTUS' and from_station = 'hgl');""")
+
+db.commit()
+c.close()
+
+"""DUMP
+echo ".mode csv
+select * from distance;" | sqlite3 data.db > distance.csv
+"""
