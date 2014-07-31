c594fe14c3e54774965aba58fdc1fe206305f22e
Thomas Koch <thakoch@gmail.com>
2014-07-31 23:13:23 +0200

    Ignore \r\n for matrix unrolling

--- unroll_matrix.py
+++ unroll_matrix.py
@@ -2,9 +2,11 @@ import sys
 
 f = open(sys.argv[1],'r')
 g = open('unrolled'+sys.argv[1],'w')
-header = f.readline()[:-1].split(',')
+header = f.readline().replace('\r\n','').split(',')
+stations = set([])
 for line in f:
-    v = line[:-1].split(',')
+    line = line.replace('\r\n','').replace('\n','')
+    v = line.split(',')
     from_station = v[0]
     for i,fare in enumerate(v[1:]):
         to_station = header[i+1]
@@ -13,5 +15,12 @@ for line in f:
             x = int(fare)
         except:
             continue
+        stations.add(to_station)
+        stations.add(from_station)
+        if len(from_station) == 0:
+            print v
+        if len(to_station) == 0:
+            print v
         g.write(','.join([from_station,to_station,fare,sys.argv[2]])+'\n')
+print stations
 g.close()
